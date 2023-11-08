e1190577a48a22b67c074f6b216866576bfd9cfe
David Akpan <Davidakpan247@gmail.com>
2023-11-08 12:07:47 -0800

    adding the model for user

--- models/user.py
+++ models/user.py
@@ -4,3 +4,18 @@ The class inherits from the BaseModel
 """
 from model.base_model import BaseModel
 
+
+class User(BaseModel):
+    """
+    The user class inherits its properties from BaseModel
+    """
+    Email = ""
+    password = ""
+    FirstName = ""
+    LastName = ""
+
+    def __init__(self arg, **kargs)
+    """
+    The init function for user
+    """
+    super().__init__(*arg, **kwargs)
